02d8a9750ad8e4331058254e26fe169730dc3ad4
Camilo Garzón <Camilogzn19@gmail.com>
2022-04-17 20:09:58 -0500

    props fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
 
 function App() {
   // Definir states
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarpregunta, actualizarPregunta] = useState(true);
+  const [gastos, guardarGastos] = useState([]);
+
+  //Cuando agregamos un nuevo gasto
+  const agregarNuevoGasto = (gasto) => {
+    console.log(gasto);
+  };
 
   return (
     <div className="container">
       <header>
         <h1>Gasto semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
-          <div className="row">
-            <div className="one-half column">1</div>
-            <div className="one-half column">2</div>
-          </div>
+          {mostrarpregunta ? (
+            <Pregunta
+              guardarPresupuesto={guardarPresupuesto}
+              guardarRestante={guardarRestante}
+              actualizarPregunta={actualizarPregunta}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario agregarNuevoGasto={agregarNuevoGasto} />
+              </div>
+              <div className="one-half column">2</div>
+            </div>
+          )}
         </div>
       </header>
     </div>
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Error from "./Error";
+import shortid from "shortid";
+
+const Formulario = ({ agregarNuevoGasto }) => {
+  // Agregar state
+
+  const [nombre, guardarNombre] = useState("");
+  const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
+
+  // Cuando el usuario agregar un gasto
+  const agregarGasto = (e) => {
+    e.preventDefault();
+
+    // Validar
+    if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    // Construir el gasto
+    const gasto = {
+      nombre,
+      cantidad,
+      id: shortid.generate(),
+    };
+    // Pasar el gasto al componente principal
+    agregarNuevoGasto(gasto);
+
+    // Resetear el form
+  };
+
+  return (
+    <form onSubmit={agregarGasto}>
+      <h2>Agrega tus gastos aquí</h2>
+      {error ? (
+        <Error mensaje="ambos campos son obligatorios o el presupuesto es incorrecto" />
+      ) : null}
+      <div className="campo">
+        <label>Nombre Gasto</label>
+        <input
+          type="text"
+          className="u-full-width"
+          placeholder="Ej. Transporte"
+          value={nombre}
+          onChange={(e) => guardarNombre(e.target.value)}
+        />
+      </div>
+      <div className="campo">
+        <label>Cantidad Gasto</label>
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Ej. 300"
+          value={cantidad}
+          onChange={(e) => guardarCantidad(parseInt(e.target.value, 10))}
+        />
+      </div>
+      <input
+        type="submit"
+        className="button-primary u-full-width"
+        value="Agregar Gasto"
+      />
+    </form>
+  );
+};
+
+export default Formulario;
